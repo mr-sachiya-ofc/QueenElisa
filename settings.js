be8983059a9247a275a1f3467005cbcb9790fe1f
mr-sachiya-ofc <130601300+mr-sachiya-ofc@users.noreply.github.com>
2024-10-14 09:22:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,58 +2,54 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: '',
-  ALIVE_REACT: '👋',
-  ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
+  SESSION_ID: 'QUEENELISA;;;==AMtNTUIFVRqh3csJHR1lTSxkUS21iUCVXQRhGVDVGR0ZWTBhUQPNTMOJUdjEVeMNEMFlUTpwd=71754666749',
+  ALIVE_REACT: '❤️',
+  ALIVE_MESSAGE: `*𝗛𝗲𝗹𝗹𝗼 𝗨𝘀𝗲𝗿 👋* *~𝗜 𝗔𝗺 𝗔𝗹𝗶𝘃𝗲 𝗡𝗼𝘄 ✅~*
 
-⏰ Now time is @time
-📅 Date : @date
+> *𝐌𝐑-𝐒𝐀𝐂𝐇𝐈𝐘𝐀 𝐌𝐃 𝐖𝐀 𝐁𝐎𝐓*
 
-🟢 I am online still @uptime
-🔄 Response Delay : @ping Ms
-💻 Host : @host
-🪢 Version : @version
+> *𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙽𝚄𝙼𝙱𝙴𝚁 +94760007746*
 
-☫ Work Mode : @mode
-☫ Work Type : @worktype
+*_❐ 𝗧𝗛𝗔𝗡𝗞𝗦 𝗙𝗢𝗥 𝗨𝗦𝗜𝗡𝗚 𝗠𝗬 𝗕𝗢𝗧 💕_*
 
-Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
-  ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
-  PREFIX: '.$&',
+
+*© ＱＵＥＥＮ-ＥＬＩＳＡ - ＭＤ  |  ２０２４*`,
+  ALIVE_MEDIA: 'https://telegra.ph/file/70444c9032ebbb2768da5.jpg',
+  MONGODB_URL: 'mongodb+srv://nimnadaanuththara62:JKTjroMYE88DmXH6@cluster0.ajet4kc.mongodb.net/', 
+  PREFIX: '.$&?',
   LANGUAGE: 'EN',
   PORT: 3000,
-  BOT_NAME: '𝐐𝐔𝐄𝐄𝐍 𝐄𝐋𝐈𝐒𝐀',
-  OWNER_NAME: 'ᴍ ʀ  ɴ ɪ ᴍ ᴀ',
-  OWNER_NUMBER: '94715166712',
-  SUDO: '94715166712',
+  BOT_NAME: '*𝐌𝐑-𝐒𝐀𝐂𝐇𝐈𝐘𝐀 𝐌𝐃*',
+  OWNER_NAME: '*ᴍ ʀ  ꜱ ᴀ ᴄ ʜ ɪ ʏ ᴀ*',
+  OWNER_NUMBER: '94760007746',
+  SUDO: '94715346004,94779164028',
   WORK_TYPE: 'public',
   WORK_MODE: 'group',
   TIME_ZONE: 'Asia/Colombo',
-  CAPTION: '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴍᴅ',
-  STICKER_AUTHOR: 'Qᴜᴇᴇɴ ᴇʟɪꜱᴀ',
-  STICKER_PACK: 'ᴍʀ ɴɪᴍᴀ ᴏꜰᴄ',
+  CAPTION: '*ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ᴍʀ-ꜱᴀᴄʜɪʏᴀ ᴍᴅ ♨*',
+  STICKER_AUTHOR: 'ꜱᴀᴄʜɪʏᴀ',
+  STICKER_PACK: 'ᴍʀ ꜱᴀᴄʜɪ ᴏꜰᴄ',
   REMOVE_BG_APIKEY: '',
-  MAXIMUM_FILESIZE: 90,
-  OWNER_R_EMOJI: '👽',
+  MAXIMUM_FILESIZE: 500,
+  OWNER_R_EMOJI: '🇰🇷',
   ALIVE_AS_GIF: true,
-  AUTO_VOICE: false,
+  AUTO_VOICE: true,
   INBOX_BLOCK: false,
   OWNER_REACT: true,
   AUTO_REACT: true,
   ONLY_PM_GROUP_MSG_SEND: false,
   AUTO_SEEN_STATUS: true,
   BOT_OFFLINE: false,
-  READ_MESSAGE: false,
+  READ_MESSAGE: true,
   ANTI_MSG_SPAM: false,
-  ANTI_BAD_WORD: false,
+  ANTI_BAD_WORD: true,
   ANTI_VIEW_ONES: true,
   ANTI_DELETE: true,
   BANED_USER_MSG_SEND: false,
   BLOCK_CMD_MSG_SEND: false,
   INBOX_BLOCK_MSG_SEND: false,
   CMD_CORRECT: true,
-  ANTI_CHANNEL_FORWARD: false,
+  ANTI_CHANNEL_FORWARD: true,
   ANTI_BOT: false,
   BUTTON_MESSAGES: true,
   SONG_DOWNLOAD_TYPE: 'selectable', // audio , document
